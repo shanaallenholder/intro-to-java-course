2635bb70c29791851c89e89f5b47eb5c7b4a68d5
Shana <shanaallenholder@hotmail.co.uk>
2023-12-28 20:55:50 +0000

    changes made

--- exercises/java-primitives/src/main/java/com/cbfacademy/strings/App.java
+++ exercises/java-primitives/src/main/java/com/cbfacademy/strings/App.java
@@ -5,9 +5,9 @@ public class App {
         System.out.println( "Hello World!" );
     }
      
-    public static String concatoString (String word1, String word2, String word3) {
+    public static String concatString (String word1, String word2, String word3) {
         // TODO: Write code that concatenates the input parameters and returns the result
-        return word1.concat(word2).concat(word3);
+        return word1.concat(word2)+ word3;
       
     }
     public static Boolean areEqual(String word1, String word2) {
@@ -18,7 +18,7 @@ public class App {
     public static String format(String item, int quantity, double price) {
         // TODO: Write code to return a string formatted as follows: "Item: [item]. Price: £[amount]. Quantity: [quantity]". The date should be formatted as DD/MM/YYYY, e.g. 24/10/2023
         //return format("item:, quantity , amount £0.0f. );
-        return "not implemented";
+        return String.format ("Item: %s. Price: £%.2f. Quantity: %d", item, price, quantity);
     }
 }
 
